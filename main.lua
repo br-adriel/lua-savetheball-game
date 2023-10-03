1e85fd101925bea74d3921d22ee9fbf5a0a0f0ab
Adriel <adriel.fsantos@outlook.com>
2023-10-02 23:58:59 -0300

    Implementa captura da posição do mouse

--- main.lua
+++ main.lua
@@ -1,13 +1,19 @@
 local love = require("love")
 
+local jogador = {
+  raio = 20,
+  x = 30,
+  y = 30,
+}
+
 function love.load()
 
 end
 
 function love.update(dt)
-
+  jogador.x, jogador.y = love.mouse.getPosition()
 end
 
 function love.draw()
-
+  love.graphics.circle("fill", jogador.x, jogador.y, jogador.raio)
 end
