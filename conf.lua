648fce07399ba37b47215880f7145c8d4e24cf33
Adriel <adriel.fsantos@outlook.com>
2023-10-03 00:11:16 -0300

    Configura janela do jogo

--- conf.lua
+++ conf.lua
@@ -2,4 +2,8 @@ function love.conf(t)
   t.version = "11.4"
 
   t.window.title = "Salve o círculo"
+  t.window.resizable = true
+  t.window.minwidth = 800
+  t.window.minheight = 600
+  t.window.highdpi = true
 end
