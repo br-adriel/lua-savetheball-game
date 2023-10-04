404160486a21c85ee917585859dc08e7e35c6cff
Adriel <adriel.fsantos@outlook.com>
2023-10-04 16:50:19 -0300

    Implementa exibição de círculos inimigos na tela

--- main.lua
+++ main.lua
@@ -1,10 +1,14 @@
 local love = require("love")
+local Inimigo = require("Inimigo")
+
+math.randomseed(os.time())
 
 local jogo = {
+  dificuldade = 1,
   estado = {
-    menu = true,
+    menu = false,
     pausado = false,
-    rodando = false,
+    rodando = true,
     finalizado = false,
   }
 }
@@ -15,12 +19,20 @@ local jogador = {
   y = 30,
 }
 
+local inimigos = {}
+
 function love.load()
   love.mouse.setVisible(false)
+
+  table.insert(inimigos, 1, Inimigo())
 end
 
 function love.update(dt)
   jogador.x, jogador.y = love.mouse.getPosition()
+
+  for i = 1, #inimigos do
+    inimigos[i]:move(jogador.x, jogador.y)
+  end
 end
 
 function love.draw()
@@ -34,6 +46,10 @@ function love.draw()
 
   if jogo.estado.rodando then
     love.graphics.circle("fill", jogador.x, jogador.y, jogador.raio)
+
+    for i = 1, #inimigos do
+      inimigos[i]:draw()
+    end
   end
 
   if not jogo.estado.rodando then
